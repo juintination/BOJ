0617c353310cad534439602052c704ef62d79293
juintination <juintin@mail.hongik.ac.kr>
2021-10-13 00:37:28 +0900

    Add : 03_For

--- C/03_For/BOJ_11_10871_X보다작은수.c
+++ C/03_For/BOJ_11_10871_X보다작은수.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 main() {
-	int n, x, y;
+	int n, x, y, cnt = 0;
 	scanf("%d %d", &n, &x);
+	int* result = (int*)malloc(sizeof(int) * n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &y);
-		if (y < x) {
-			printf("%d\n", y);
-		}
+		if (y < x) result[cnt++] = y;
+	}
+	for (int i = 0; i < cnt; i++) {
+		printf("%d ", result[i]);
 	}
 }
