1857c0b683a7bf38d68ef867ab89f859187a116e
juintination <juintin@mail.hongik.ac.kr>
2021-10-13 00:29:00 +0900

    Add : 04_While

--- JAVA/04_While/BOJ_02_10951_A+B-4.java
+++ JAVA/04_While/BOJ_02_10951_A+B-4.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,8 +9,9 @@ public class Main {
         String str;
         StringBuilder sb = new StringBuilder();
         while ((str = br.readLine()) != null) {
-            int x = str.charAt(0) - '0';
-            int y = str.charAt(2) - '0';
+            StringTokenizer st = new StringTokenizer(str, " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             sb.append(x + y).append("\n");
         }
         System.out.print(sb);
