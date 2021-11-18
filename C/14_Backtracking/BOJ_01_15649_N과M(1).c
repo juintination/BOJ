89e631a1c8ffeeedc575affbe1706c59d201da9d
juintination <juintin@mail.hongik.ac.kr>
2021-11-18 20:23:37 +0900

    Add : 14_Backtracking

---
+++ C/14_Backtracking/BOJ_01_15649_N과M(1).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int arr[8], visited[8], n, m;
+void dfs(int dpth) {
+	if (dpth == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (visited[i] == 0) {
+			visited[i] = !visited[i];
+			arr[dpth] = i + 1;
+			dfs(dpth + 1);
+			visited[i] = !visited[i];
+		}
+	}
+}
+main() {
+	scanf("%d %d", &n, &m);
+	dfs(0);
+}
