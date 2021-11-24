d1dbdbf3d113b942d9248a01b7dd9f3bde1deb42
juintination <juintin@mail.hongik.ac.kr>
2021-11-24 18:55:16 +0900

    Add : 14_Backtracking

---
+++ C/14_Backtracking/BOJ_10973_이전순열.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+main() {
+	int n;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int i = n - 1;
+	while (1) {
+		if (i == 0 || arr[i - 1] > arr[i]) break;
+		i--;
+	}
+	if (i == 0) {
+		printf("-1");
+		return 0;
+	}
+	int j = n - 1;
+	while (1) {
+		if (arr[i - 1] > arr[j]) break;
+		j--;
+	}
+	swap(&arr[i - 1], &arr[j]);
+	int k = n - 1;
+	while (i < k) {
+		swap(&arr[i], &arr[k]);
+		i++;
+		k--;
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+}
---
+++ JAVA/14_Backtracking/BOJ_10973_이전순열.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+
+    public static void swap(int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static boolean previousPermutation() {
+        int i = n - 1;
+        while (true) {
+            if (i == 0 || arr[i - 1] > arr[i]) break;
+            i--;
+        }
+        if (i == 0) {
+            System.out.println(-1);
+            return false;
+        }
+        int j = n - 1;
+        while (true) {
+            if (arr[i - 1] > arr[j]) break;
+            j--;
+        }
+        swap(i - 1, j);
+        int k = n - 1;
+        while (i < k) {
+            swap(i, k);
+            i++;
+            k--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        if (previousPermutation()) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            System.out.print(sb);
+        }
+    }
+}
