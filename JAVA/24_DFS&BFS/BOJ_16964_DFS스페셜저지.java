34c9d6a1a3a41d77cb6e83bf8dad925613ecae9f
juintination <juintin@mail.hongik.ac.kr>
2021-12-24 16:44:33 +0900

    Add : 24_DFS&BFS

---
+++ JAVA/24_DFS&BFS/BOJ_16964_DFS스페셜저지.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, cnt = 0;
+    static int[] arr, order;
+    static boolean[] visited;
+    static ArrayList<Integer>[] list;
+
+    public static void dfs(int idx) {
+        if (idx != arr[cnt++]) {
+            System.out.println(0);
+            System.exit(0);
+        }
+        visited[idx] = true;
+        for (int e : list[idx]) {
+            if (!visited[e]) {
+                dfs(e);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n + 1];
+        list = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list[x].add(y);
+            list[y].add(x);
+        }
+        arr = new int[n];
+        order = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            arr[i] = tmp;
+            order[tmp] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (list[i] != null) {
+                Collections.sort(list[i], new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return order[o1] - order[o2];
+                    }
+                });
+            }
+        }
+        dfs(1);
+        System.out.println(1);
+    }
+}
