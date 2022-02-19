7d2a08c8202129e962af2e2813b600aef8e11e2e
juintination <juintin@mail.hongik.ac.kr>
2022-02-20 00:10:13 +0900

    Fix : This problem was re-scored

--- C/12_Sort/BOJ_06_11650_좌표정렬하기.c
+++ C/12_Sort/BOJ_06_11650_좌표정렬하기.c
@@ -1,41 +1,23 @@
 #include <stdio.h>
-void quickSort(int arrx[], int arry[], int left, int right) {
-	int i = left, j = right, pivot = arrx[(left + right) / 2], temp;
-	do {
-		while (arrx[i] < pivot) i++;
-		while (arrx[j] > pivot) j--;
-		if (i <= j) {
-			temp = arrx[i];
-			arrx[i] = arrx[j];
-			arrx[j] = temp;
-			temp = arry[i];
-			arry[i] = arry[j];
-			arry[j] = temp;
-			i++;
-			j--;
-		}
-	} while (i <= j);
-	if (left < j) quickSort(arrx, arry, left, j);
-	if (i < right) quickSort(arrx, arry, i, right);
+#include <stdlib.h>
+typedef struct {
+	int x, y;
+} point;
+int compare(const void* a, const void* b) {
+	point o1 = *(point*)a;
+	point o2 = *(point*)b;
+	if (o1.x == o2.x) return o1.y - o2.y;
+	else return o1.x - o2.x;
 }
 main() {
-	int x[100000], y[100000], n, tmp;
+	int n;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) scanf("%d %d", &x[i], &y[i]);
-	quickSort(x, y, 0, n - 1);
-	while (1) {
-		int cnt = 0;
-		for (int i = 0; i < n - 1; i++) {
-			if (x[i] == x[i + 1]) {
-				if (y[i] > y[i + 1]) {
-					tmp = y[i + 1];
-					y[i + 1] = y[i];
-					y[i] = tmp;
-					cnt++;
-				}
-			}
-		}
-		if (cnt == 0) break;
+	point* arr = (point*)malloc(sizeof(point) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
 	}
-	for (int i = 0; i < n; i++) printf("%d %d\n", x[i], y[i]);
+	qsort(arr, n, sizeof(point), compare);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
 }
--- C/12_Sort/BOJ_07_11651_좌표정렬하기2.c
+++ C/12_Sort/BOJ_07_11651_좌표정렬하기2.c
@@ -1,42 +1,23 @@
 #include <stdio.h>
-void quickSort(int arrx[], int arry[], int left, int right) {
-	int i = left, j = right, pivot = arry[(left + right) / 2], temp;
-	//arrx로 하면 계속 원위치돼서 무한루프가 된다
-	do {
-		while (arry[i] < pivot) i++;
-		while (arry[j] > pivot) j--;
-		if (i <= j) {
-			temp = arrx[i];
-			arrx[i] = arrx[j];
-			arrx[j] = temp;
-			temp = arry[i];
-			arry[i] = arry[j];
-			arry[j] = temp;
-			i++;
-			j--;
-		}
-	} while (i <= j);
-	if (left < j) quickSort(arrx, arry, left, j);
-	if (i < right) quickSort(arrx, arry, i, right);
+#include <stdlib.h>
+typedef struct {
+	int x, y;
+} point;
+int compare(const void* a, const void* b) {
+	point o1 = *(point*)a;
+	point o2 = *(point*)b;
+	if (o1.y == o2.y) return o1.x - o2.x;
+	else return o1.y - o2.y;
 }
 main() {
-	int x[100000], y[100000], n, tmp;
+	int n;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) scanf("%d %d", &x[i], &y[i]);
-	quickSort(x, y, 0, n - 1);
-	while (1) {
-		int cnt = 0;
-		for (int i = 0; i < n - 1; i++) {
-			if (y[i] == y[i + 1]) {
-				if (x[i] > x[i + 1]) {
-					tmp = x[i + 1];
-					x[i + 1] = x[i];
-					x[i] = tmp;
-					cnt++;
-				}
-			}
-		}
-		if (cnt == 0) break;
+	point* arr = (point*)malloc(sizeof(point) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].x, &arr[i].y);
 	}
-	for (int i = 0; i < n; i++) printf("%d %d\n", x[i], y[i]);
+	qsort(arr, n, sizeof(point), compare);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
 }
