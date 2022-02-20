28625c048b7eddd8c740f3aede46467dee177dcf
juintination <juintin@mail.hongik.ac.kr>
2022-02-20 18:38:07 +0900

    Add : 12_Sort

---
+++ C/12_Sort/BOJ_11931_수정렬하기-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void* a, const void* b) {
+	int o1 = *(int*)a;
+	int o2 = *(int*)b;
+	if (o1 > o2) return 1;
+	else if (o1 < o2) return -1;
+	else return 0;
+}
+main() {
+	int n;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	qsort(arr, n, sizeof(int), compare);
+	for (int i = n - 1; i >= 0; i--) {
+		printf("%d\n", arr[i]);
+	}
+}
---
+++ JAVA/12_Sort/BOJ_11931_수정렬하기-4.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i = n - 1; i >= 0; i--) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
